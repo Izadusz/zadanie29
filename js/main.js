287679c70f1db899fb75328d98c4b8e374e0d0a2
Iza Duszynska <iza.duszynska@wp.pl>
2020-09-02 19:38:48 +0200

    zadanie29

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 4;
+let secondNumber = 2;
+
+function addNumbers(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+console.log(addNumbers(firstNumber, secondNumber));
